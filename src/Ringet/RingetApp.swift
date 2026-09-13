aec995854c5ac7121235e59db237903eb9961654
Martin Kearn <martinkearn@live.co.uk>
2026-09-13 08:30:27 +0100

    Initial project

---
+++ src/Ringet/RingetApp.swift
@@ -0,0 +1,17 @@
+//
+//  RingetApp.swift
+//  Ringet
+//
+//  Created by Martin Kearn on 13/09/2026.
+//
+
+import SwiftUI
+
+@main
+struct RingetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
